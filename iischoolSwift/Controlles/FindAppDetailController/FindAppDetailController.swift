39d675681d56bd61dd9d5218af9faccef46ef802
Dudu <dududyx@163.com>
2018-12-18 15:32:52 +0800

    增加一些控制器

---
+++ iischoolSwift/Controlles/FindAppDetailController/FindAppDetailController.swift
@@ -0,0 +1,30 @@
+//
+//  FindAppDetailController.swift
+//  iischoolSwift
+//
+//  Created by junke on 2018/12/18.
+//  Copyright © 2018 hank. All rights reserved.
+//
+
+import UIKit
+
+class FindAppDetailController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
